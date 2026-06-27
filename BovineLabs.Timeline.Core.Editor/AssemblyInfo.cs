a779d2c5752321b01278269deb9045e21306d317
IAFahim <iafahim.dev@gmail.com>
2026-06-27 17:03:01 +0600

    Fixed input

--- BovineLabs.Timeline.Core.Editor/AssemblyInfo.cs
+++ BovineLabs.Timeline.Core.Editor/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("BovineLabs.Timeline.Essence.Editor")]
 [assembly: InternalsVisibleTo("BovineLabs.Timeline.EntityLinks.Editor")]
 [assembly: InternalsVisibleTo("BovineLabs.Timeline.Physics.Editor")]
+[assembly: InternalsVisibleTo("BovineLabs.Timeline.Audio.Editor")]
